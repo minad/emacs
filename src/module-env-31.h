25252b7cf57e8cd8df1fa83baf175b60a1064691
Divya Ranjan <divya@subvertising.org>
2026-07-08 07:20:39 +0000

    New image type canvas
    Canvas images support efficient updating and refreshing.  An image
    specifier with ':type canvas' creates a canvas image.  The canvas has an
    associated writable pixel buffer.  After changing the ':data' vector or
    writing to the pixel buffer a call to 'canvas-refresh' redraws the
    canvas image.  See bug#80281.
    
    Co-Authored-By: Divya Ranjan <divya@subvertising.org>
    Co-Authored-By: Daniel Mendler <mail@daniel-mendler.de>
    
    Canvas changes:
    
    * doc/lispref/display.texi (Images): Add Canvas Images to menu.
    (Canvas Images): New node documenting canvas image objects, the
    `:data-width', `:data-height', `:data' and `:file' properties, and
    `canvas-refresh'.
    * doc/lispref/elisp.texi (Top): Reference new Canvas Images node.
    * doc/lispref/internals.texi (Writing Dynamic Modules): Add Module
    Canvas API to menu.
    (Module Canvas API): New node documenting `canvas_data' module function.
    * etc/NEWS: Announce the addition.
    * lisp/image.el (create-image): Add `:id' to image specification of
    canvas images.
    * src/dispextern.h (struct image): Add `refresh', `original_width' and
    `original_height' fields.
    (redraw_image_glyphs, canvas_data): New prototypes.
    * src/emacs-module.c (module_canvas_data): New function.
    (initialize_environment): Register `canvas_data' module function.
    * src/image.c (struct canvas): New struct for canvas.
    (canvas_prepare_for_display, canvas_free_unused): New forward
    declarations.
    (prepare_image_for_display): Call `canvas_prepare_for_display' when the
    canvas refresh counter has changed.  Set image `original_width' and
    `original_height'.
    (filter_image_spec): Exclude `:data' from the image cache key for canvas
    images, so mutating the data vector in place does not create a new cache
    entry.
    (Fclear_image_cache): Call `canvas_free_unused'.
    (image_set_transform): Use CAIRO_FILTER_GOOD instead of
    CAIRO_FILTER_BEST for canvas images, for performance.  Do not set image
    `original_width' and `original_height'.
    (canvas_image_p, canvas_free_unused, canvas_apply_data, canvas_get)
    (canvas_parse, canvas_load, canvas_prepare_for_display, canvas_data):
    New functions implementing canvas creation, resizing, and reloading of
    `:data' or `:file' content.
    (Fcanvas_refresh): New defun `canvas-refresh', with an optional
    RELOAD-DATA argument to reread `:data' or `:file'.
    (initialize_image_type, syms_of_image): Register the `canvas' image type
    and `canvas-refresh'.
    * src/module-env-31.h (canvas_data): New prototype.
    * src/nsimage.m (ns_image_reset): New function to recreate an NS pixmap
    to update the canvas.
    * src/nsterm.h: Add prototype for `ns_image_reset'.
    * src/xdisp.c (redraw_image_glyphs_window)
    (redraw_image_glyphs_window_tree, redraw_image_glyphs): New functions to
    redraw only the glyphs referencing a given canvas image spec, falling
    back to a full frame redraw if the frame is garbaged.
    * test/src/emacs-module-resources/mod-test.c (mod-test-canvas-read)
    (mod-test-canvas-write, mod-test-canvas-invalid): New helper functions.
    * test/src/emacs-module-tests.el (test-canvas-gen-file)
    (mod-test-canvas/valid, mod-test-canvas/invalid, mod-test-canvas/vector)
    (mod-test-canvas/vector-reload, mod-test-canvas/unibyte)
    (mod-test-canvas/file, mod-test-canvas/gc-stress): New tests.

--- src/module-env-31.h
+++ src/module-env-31.h
@@ -1,3 +1,12 @@
   /* Add module environment functions newly added in Emacs 31 here.
      Before Emacs 31 is released, remove this comment and start
      module-env-32.h on the master branch.  */
+
+  /* Get pointer to the pixel buffer of CANVAS.  The buffer is in row
+     major order and has the size width * height.  The pixel format is
+     ARGB32.  The pointer will be valid as long as CANVAS is alive,
+     and as long as its dimensions have not been changed.
+     Return NULL in case error.   */
+
+  uint32_t* (*canvas_data) (emacs_env *env, emacs_value canvas)
+    EMACS_ATTRIBUTE_NONNULL(1);
